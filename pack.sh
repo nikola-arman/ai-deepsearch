ee3c6ec086b4a17f4a5f7976225b78dba8d097c1
reinSPQR <rein1605@bvm.network>
2025-08-19 17:17:06 +0700

    Fix pack.sh

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 rm -rf ai-deepsearch.zip
-zip -r ai-deepsearch.zip app deepsearch requirements.txt Dockerfile system_prompt.txt
+zip -r ai-deepsearch.zip app deepsearch server.py requirements.txt Dockerfile system_prompt.txt
